23019a94415ad4ffe500f23c190cab14629cd505
doombubbles <obsidiantoolzg@gmail.com>
2023-06-07 09:40:16 -0700

    In progress

---
+++ Tier6.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using BTD_Mod_Helper.Api.Enums;
+using BTD_Mod_Helper.Extensions;
+using Il2Cpp;
+using Il2CppAssets.Scripts.Models.GenericBehaviors;
+using Il2CppAssets.Scripts.Models.Towers;
+using Il2CppAssets.Scripts.Models.Towers.Filters;
+using Il2CppAssets.Scripts.Models.Towers.Projectiles.Behaviors;
+using Il2CppAssets.Scripts.Unity;
+using PathsPlusPlus;
+
+namespace TornadoWizards;
+
+public class Sharknado : UpgradePlusPlus<TornadoWizardPath>
+{
+    public override int Cost => 100000;
+    public override int Tier => 6;
+    public override string Icon => VanillaSprites.MegalodonUpgradeIcon;
+    public override string Portrait => "Wizard5";
+
+    public override string Description => "Sharknado lol.";
+
+    public override void ApplyUpgrade(TowerModel towerModel)
+    {
+        
+    }
+}
--- TornadoWizardsMod.cs
+++ TornadoWizardsMod.cs
@@ -1,6 +1,10 @@
 using MelonLoader;
 using BTD_Mod_Helper;
+using BTD_Mod_Helper.Api.Enums;
+using BTD_Mod_Helper.Extensions;
 using Il2CppAssets.Scripts.Models.Towers;
+using Il2CppAssets.Scripts.Models.Towers.Behaviors;
+using Il2CppAssets.Scripts.Utils;
 using PathsPlusPlus;
 using TornadoWizards;
 
@@ -16,5 +20,29 @@ public class TornadoWizardsMod : BloonsTD6Mod
 public class TornadoWizardPath : PathPlusPlus
 {
     public override string Tower => TowerType.WizardMonkey;
-    public override int UpgradeCount => 5;
+    public override int UpgradeCount => 6;
+}
+
+public class WizardBottomPath : PathPlusPlus
+{
+    public override string Tower => TowerType.WizardMonkey;
+    public override int UpgradeCount => 6;
+    public override int ExtendVanillaPath => Bottom;
+}
+
+public class EmperorOfDarkness : UpgradePlusPlus<WizardBottomPath>
+{
+    public override int Cost => 50000;
+    public override int Tier => 6;
+
+    public override string Icon => VanillaSprites.SoulbindUpgradeIcon;
+    public override string? Portrait => VanillaSprites.Wizard005;
+
+    public override string Description => "Global zombie buff better";
+
+    public override void ApplyUpgrade(TowerModel towerModel)
+    {
+        var buffModel = towerModel.GetBehavior<PrinceOfDarknessZombieBuffModel>();
+        buffModel.damageIncrease += 4;
+    }
 }
